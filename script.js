1acd61999a4b5dcee921d1f084060c77c25c30eb
daniels <danielsv2008@gmail.com>
2025-09-19 13:43:36 +0300

    html fixed, input for posts added, ul added, status Loading..

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const btn = document.getElementById("btn");
 const status = document.getElementById("status");
 const posts = document.getElementById("posts-list")
+const number = document.getElementById("number");
 
 async function getPosts() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await res.json();
 
     console.log(data);
+}
+
+function postList() {
+
 }
 
 btn.addEventListener("click", getPosts);
