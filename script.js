8b56dfaf4dc886d49c2252e89d616ae9a96da86b
daniels <danielsv2008@gmail.com>
2025-09-19 13:32:23 +0300

    async function get posts added, jaunie mainigie pievienoti

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById("btn");
+const status = document.getElementById("status");
+const posts = document.getElementById("posts-list")
 
-async function changeNumber(){
-    const res = await fetch("");
+async function getPosts() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await res.json();
-    
+
     console.log(data);
 }
 
-btn.addEventListener("click", changeNumber);
+btn.addEventListener("click", getPosts);
